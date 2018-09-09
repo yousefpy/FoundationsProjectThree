3bdf6bbdbe875c1d8e8711059a3fc6a54c23528f
darkwight <m.alsharekh94@gmail.com>
2018-09-09 18:37:42 +0300

    Fixed lil bug

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 # CREATION OF DATA
 ######################### DO NOT MODIFY THIS CODE ###########################
-from components import Population, Person, Club
+from components import Person, Club
 steve = Person("Steve", "average joe", 27)
 michelle = Person("Michelle", "average jane", 12)
 john = Person("John", "a blond guy", 32)
